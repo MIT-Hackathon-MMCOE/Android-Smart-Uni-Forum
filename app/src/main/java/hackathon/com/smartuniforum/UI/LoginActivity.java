2ab79b396b4b402c5e7cb20ba6d8ee7e1c53e1bb
Aditya Kulkarni <kulkarniaditya1997@gmail.com>
2018-02-16 14:21:39 +0530

    Activity with login and signup tabs

---
+++ app/src/main/java/hackathon/com/smartuniforum/UI/LoginActivity.java
@@ -0,0 +1,61 @@
+package hackathon.com.smartuniforum.UI;
+
+import android.support.design.widget.TabLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.TextView;
+
+import hackathon.com.smartuniforum.Adapter.SectionPagerAdapter;
+import hackathon.com.smartuniforum.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private SectionPagerAdapter mSectionsPagerAdapter;
+
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        mSectionsPagerAdapter = new SectionPagerAdapter(getSupportFragmentManager());
+
+        mViewPager = findViewById(R.id.container);
+        mSectionsPagerAdapter.addFragments(new LoginFragment());
+        mSectionsPagerAdapter.addFragments(new SignUpFragment());
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        TabLayout tabLayout = findViewById(R.id.tabs);
+
+        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
