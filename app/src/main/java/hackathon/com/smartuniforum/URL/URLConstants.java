0dcc00e9b788f83e6a98414eb089d0a8e6baef37
Aditya Kulkarni <kulkarniaditya1997@gmail.com>
2018-02-17 14:22:04 +0530

    Add files via upload

--- app/src/main/java/hackathon/com/smartuniforum/URL/URLConstants.java
+++ app/src/main/java/hackathon/com/smartuniforum/URL/URLConstants.java
@@ -1,1 +1,16 @@
+package hackathon.com.smartuniforum.URL;
 
+/**
+ * Created by aditya on 16/2/18.
+ */
+
+public class URLConstants {
+
+    public static String BASE_URL = "http://10.42.0.1:8000/";
+
+    public static String LOGIN_URL = "/rest-auth/login/";
+
+    public static String SIGN_UP_URL = "/rest-auth/registration/";
+
+    public static String GET_PROFILE_URL = "/rest-auth/";
+}
